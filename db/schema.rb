71580c12d63d3c0af54b8b1db620ba7d45ee11d3
Kyle L. Cleaver <kyle.Cleaver@careerbuilder.com>
2015-12-05 17:14:33 -0500

    add category object

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205185146) do
+ActiveRecord::Schema.define(version: 20151205220257) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "equipment", force: :cascade do |t|
     t.string   "name"
