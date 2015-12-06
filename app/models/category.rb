ea77f71bc633b1f24b29bdecb8839bc9f30035e3
Kyle L. Cleaver <kyle.Cleaver@careerbuilder.com>
2015-12-05 21:07:16 -0500

    validates and associate category and equipment

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
 	has_many :equipment
+	validates_presence_of :name, :description
+	validates_uniqueness_of :name
 end
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,4 +1,6 @@
 class Equipment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :category
+	validates_presence_of :name, :condition, :estimated_value, :category, :purchase_date, :city, :state
+	validates_uniqueness_of :name
 end
